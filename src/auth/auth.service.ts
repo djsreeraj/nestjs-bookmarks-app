f758cfa18e368259ad7a0161fd63032d6bf006d0
djsreeraj <sreeraj011m@gmail.com>
2022-05-04 14:49:10 +0530

    added basic controllers and db configs

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+
+@Injectable()
+export class AuthService {
+    constructor(private prisma: PrismaService){
+
+    }
+    signup(){
+        return { msg: 'I have signed up!'};
+    }
+
+    login(){
+        return { msg: 'I have logged in..!'};
+    }
+}
