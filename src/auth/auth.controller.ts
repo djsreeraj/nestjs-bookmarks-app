09a51b12b2d1252a7553bd5713c33efb9b48cc59
djsreeraj <sreeraj011m@gmail.com>
2022-05-08 18:42:17 +0530

    added validation using pipe -  class validator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
-import { Controller, Post, Req } from "@nestjs/common";
-import { Request } from "express";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { AuthDto } from "./dto";
 
 @Controller('auth')
 export class AuthController{
     constructor(private authService: AuthService) {}
+    
     @Post('signup')
-    signup(@Req() req: Request) {
-        console.log(req.body)
+    signup(@Body() dto: AuthDto) {
+        console.log({dto})
         return this.authService.signup()
     }
     @Post('login')
