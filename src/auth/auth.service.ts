995349c1707af97f66b1542a932f366d08e07d62
djsreeraj <sreeraj011m@gmail.com>
2022-05-12 11:35:20 +0530

    improved token response

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -56,19 +56,24 @@ export class AuthService {
 
     }
 
-    signToken(
+    async signToken(
         userId:number,
         email:string
-    ) : Promise<string>{
+    ) : Promise<{access_token :string}>{
         const payload = {
             sub: userId,
             email
         }
 
         const secret = this.config.get('JWT_SECRET')
-        return this.jwt.signAsync(payload, {
+        const token = await this.jwt.signAsync(payload, {
             expiresIn:'15m',
             secret: secret
-        })
+        });
+
+        return {
+            access_token: token,
+        }
+
     }
 }
