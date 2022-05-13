4e7c5165dc8e2580d1fdf978aff75680f612033d
djsreeraj <sreeraj011m@gmail.com>
2022-05-14 00:18:58 +0530

    added missing validate in jwt stategy

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -4,12 +4,18 @@ import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy){
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt'){
     constructor(config: ConfigService){
         super({
         jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
         ignoreExpiration: false,
         secretOrKey: config.get('JWT_SECRET'),
         });
+    }
+
+    validate(payload: any){
+        console.log({ payload });
+
+        return payload;
     }
 }
